28b3284c5401a718243b2740465dfe1ece148126
Samuel Martin <Samuel Martin>
2018-07-24 11:05:42 +0100

    Added a text-based filepicker (as module)

--- plagcheck.py
+++ plagcheck.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# Must be in same folder as the essay, reading_pack and
+# filepicker.py (my own module)
+
 # to check for plagiarism between 2 documents:
 #              reading_pack.txt and essay.txt
 
@@ -8,9 +11,6 @@
         for each occurance of word in essay:
             for each occurance of word in reading pack:
                 check for matches in texts.    """
-# Still to do:
-#   remove non-content words from unique_words
-#   make the program work with pdf, doc and docx files
 
 def main():
     level = welcome() #working
@@ -40,8 +40,10 @@ def get_texts():
     returning clean texts, a list of unique words, and the original essay"""
 
     def get_text_names(): # please develop to allow selection of text
-        print("\nThe files being opened must be named\n'reading_pack.txt' and 'essay.txt'\n")
-        texts = ['reading_pack.txt', 'essay.txt']
+        import filepicker # my own module. Must be in the same directory
+        reading_pack = filepicker.filepicker('reading pack') # user input
+        essay = filepicker.filepicker('essay') # user input
+        texts = [reading_pack, essay]
         return texts
 
     def open_text(text): # used to open files - txt only for now
@@ -65,7 +67,6 @@ def get_texts():
 
         # remove common words to save processing
         # UNDER CONSTRUCTION
-        print("\nORIGINAL ESSAY:      ", all_essay_words)
         stoplist = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn']
         for stop in stoplist:
             while stop in all_essay_words:
@@ -144,6 +145,7 @@ def remove_substrings(duplicates):
     for reject in rejects:#remove substrings from the original list.
         duplicates.remove(reject)
     #print('\nduplicates feturned by "remove_substrings()" = \n{}\n'.format(duplicates))#test only#               
+    print('\nHere are the phrases in the essay, taken from the reading pack:\n')
     print(duplicates)
     return duplicates
 
