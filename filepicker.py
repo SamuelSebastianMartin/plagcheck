330db2b60129dd6b9013ec9d253341937e991a3c
Samuel Martin <Samuel Martin>
2018-07-24 22:11:19 +0100

    Improvements in style only after using 'pycodestyle'

--- filepicker.py
+++ filepicker.py
@@ -4,11 +4,12 @@
 
 import os
 
+
 def lsPrint():
     '''Prints a numbered list of useful files in cwd.
     Only those beginning with a letter
     and which have a '.' in them.'''
-    print()    
+    print()
     # get list of relevant files (not .file or directories)
     ls = os.listdir()
     dirList = []
@@ -22,18 +23,20 @@ def lsPrint():
     print()
     return dirList
 
+
 def inputSentinelInt(From=0, To=999, message='Please pick a number: '):
     ''''Provides a sentinel restriction on input
     to avoid non-integers or out-of-range values.
     Defaults are between From 0 To 999
-    but these can be changed with 
+    but these can be changed with
     "From=-1, To=8" for example
     There is an optional "message=" argument, too -
     the default is "Please pick a number".
     It returns the number as int'''
-    number = 'a' # dummy value
+    number = 'a'  # dummy value
     # control imput to numer withing correct range
-    while number.isnumeric() == False or int(From) > int(number) or int(number) > int(To):
+    while not number.isnumeric() or int(From) > int(number) \
+                                 or int(number) > int(To):
         number = input(message)
     number = int(number)
     return number
@@ -48,8 +51,8 @@ def filepicker(requested_file='file'):
     would yield: "Please chose the NUMBER of your input file: "
     '''
     print('\nPlease choose one of the following files: ')
-    dirList = lsPrint() # list relevant files
-    #set values for input sentinel funtion
+    dirList = lsPrint()  # list relevant files
+    # set values for input sentinel funtion
     lower = 0
     upper = len(dirList)-1
     message = 'Please chose the NUMBER of your {}: '.format(requested_file)
@@ -61,10 +64,12 @@ def filepicker(requested_file='file'):
 
     return filename
 
+
 def basic():
     '''Like 'filepicker()' but with no controll over the message. Always gives
     "Please give the number of your file: "'''
     filename = filepicker()
+
 
 if __name__ == '__basic__':
     basic()
