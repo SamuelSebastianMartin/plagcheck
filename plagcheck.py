1cb6d89553b154a30c8c19a347358ddc775f3e89
Samuel Martin <Samuel Martin>
2018-08-05 21:52:09 +0100

    Added result filter() and result() present fns; now returns strings over critial length.

--- plagcheck.py
+++ plagcheck.py
@@ -20,11 +20,9 @@ def main():
     textnames = get_text_names()
     rp, sa = get_texts.get_texts(textnames)
     duplicates = find_matches.find_matches(rp, sa)
-    print('These are the duplicated words and phrases')
-    for result in duplicates:
-        critical_length = 2
-        if len(result) > critical_length:
-            print(result)
+    shortlist = filter(duplicates)
+    results(shortlist)
+
 
 def welcome():
     print("""
@@ -42,6 +40,15 @@ def get_text_names():
     texts = [reading_pack, essay]
     return texts
 
+def filter(duplicates):
+    '''Filters the results'''
+    critical = 3  # Minimum number of plag words to check for.
+    shortlist = [phrs for phrs in duplicates if len(phrs) >= critical]
+    return shortlist
+
+def results(shortlist):
+    for result in shortlist:
+        print(' '.join(result))
 
 
 if __name__ == '__main__':
