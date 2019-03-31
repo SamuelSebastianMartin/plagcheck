51496a93d1b3adb5a37527f48533bb388c387db1
Samuel Martin <Samuel Martin>
2019-03-31 18:54:38 +0100

    updated 'match_class': part of refactoring

--- match_class.py
+++ match_class.py
@@ -1,27 +1,34 @@
 #! /usr/bin/env python3
 
+import re
+
+
 class Match:
 
-    def __init__(self, list_span, rp, para_words, text):
-        self.list_span = list_span
-        self.text_span = get_text_span(list_span, para_words, text)
-        regex = get_regex(self.span, para_words)
+    def __init__(self, i, para_words, text):
+        self.i = i
+        self.j = i + 1  # 3 is the minimum word match requirement.
+        self.regex = self.get_regex(para_words)
+        self.found = self.search(text)
 
-    def get_regex(self, list_span, para_words):
+    def get_regex(self, para_words):
         """produces the regex search for any span in para_words"""
-        str = r'\W+'.join(para_words[self.span[0]: self.span[1]])
+        str = r'\W+'.join(para_words[self.i : self.j])
         expr = re.compile(str, re.IGNORECASE)
         return expr
 
-    def set_list_span(self):
-        self.list_span = (a, b)
-
-    def get_text_span(self, list_span, para_words, text):
-        return (0, 0)
+    def search(self, text):
+        srch = self.regex.search(text)
+        return srch
 
 
 def test():
-    pass
+    a = ['one', 'two', 'three', 'four', 'five', 'six', 'seven']
+    b = 'three four five'
+    match = Match(2, a, b)
+
+    if match.found:
+        print('found', match.found.group())
 
 
 if __name__ == '__main__':
